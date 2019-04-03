7b2f5d7b695df32bf7cf3701a57712cb60db4592
Selena Solis <lenansolis@gmail.com>
2019-04-03 18:13:37 -0500

    html and css

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,48 +1,47 @@
 'use strict';
 
 // subtract one number from another
-var firstNumber = 9;
-var secondNumber = 3;
-console.log(firstNumber - secondNumber);
+var firstNum = 9;
+var secondNum = 3;
+console.log(firstNum - secondNum);
 
 //Display the current day and time in javascript
 var date = new Date();
-console.log(date.toLocaleString('en-us'));
+document.getElementById('dateTime').innerHTML = date.toLocaleString('en-us')
 
 
 //Convert a number, 7 to a string, "7" in javascript
 var num = 7;
 var str = String(num);
-console.log(str);
+document.getElementById('numToStr').innerHTML = typeof str;
 
 //Convert a string, "7" to the number, 7 in javascript
 var str = '7';
 var num = Number(str);
-console.log(num);
+document.getElementById('strToNum').innerHTML = typeof num;
 
 //Add 2 numbers together in javascript
 var num1 = 10;
 var num2 = 5;
-console.log(num1+num2);
+document.getElementById('sumTwoNum').innerHTML = num1 + num2;
 
 //Prints out "Both are TRUE" only when 2 things are true
-var x = true;
-var y = true;
-var z = false;
-if(x  && y ){
-  console.log("Both are TRUE");
+var a = true;
+var b = true;
+var c = false;
+var d = false;
+if(a  && b ){
+  document.getElementById('bothTrue').innerHTML = "Both are TRUE";
 }
 
 //Prints out "One of these is TRUE" when 1 of 2 things are true.
-if (x || z){
-  console.log("One of these is TRUE");
+if (a || c){
+  document.getElementById('oneTrue').innerHTML = "One of these is TRUE";
 }
 
 //Prints out "Neither is TRUE" when both things are not true.
-var a = false;
-var b = false;
-if (!a && !b){
-  console.log("Neither is TRUE");
+if (!c && !d){
+  document.getElementById('noneTrue').innerHTML = "Neither is TRUE";
 }
 
 
@@ -54,9 +53,14 @@ if (!a && !b){
 //NaN
 //String
 var myBool = true;
+document.getElementById('myBool').innerHTML = typeof myBool;
 var myNull = null;
+document.getElementById('myNull').innerHTML = typeof myNull;
 var myUnd;
+document.getElementById('myUnd').innerHTML = typeof myUnd;
 var myNum = 10;
-var myNan = 10/0;
-var myStr = "";
-console.log(typeof(myNull));
+document.getElementById('myNum').innerHTML = typeof myNum;
+var myNan = 0/0;
+document.getElementById('myNan').innerHTML = typeof myNan;
+var myStr = "poop";
+document.getElementById('myStr').innerHTML = typeof myStr;
