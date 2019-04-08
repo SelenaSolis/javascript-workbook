115ba1f572720fb021fed2ad2b12584e2b898f63
Selena Solis <lenansolis@gmail.com>
2019-04-08 15:29:54 -0500

    initial

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,8 +9,50 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
+  //trims input word
+  word = word.trim();
 
-  // Your code here
+  //converts word to lowercase
+  word = word.toLowerCase();
+
+  //array of vowels
+  var vowelArr = ["a", "e", "i", "o", "u", "y"];
+
+  //variable to hold letters of word until vowel
+  var vowel = "b";
+
+  //variable to count position of vowel
+  var i = 0;
+
+  //while loop to look through word until a vowel
+    while(!vowelArr.includes(vowel)){
+      //assign letter at position i to vowel
+      vowel = word.charAt(i);
+      //increment position by 1
+      i = i + 1;
+    }
+
+    //variable for first instance of a vowel
+    i = i - 1;
+
+    //variable for the letters before the vowel
+    var beg = word.slice(0, i);
+
+    //if word starts with a vowel add "yay"
+
+    if (vowelArr.includes(word.charAt(0))){
+      //removes letters before vowel and concatenates them to the end with "yay"
+      word = word.slice(i) + beg + 'yay';
+    }
+    //if not add "ay"
+    else{
+      //removes letters before vowel and concatenates them to the end with "ay"
+      word = word.slice(i) + beg + 'ay';
+    }
+    return word;
+  
+
+  //add 'ay' to the end
 
 }
 
