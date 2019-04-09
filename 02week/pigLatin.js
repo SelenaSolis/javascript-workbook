189f841332ef27ca4c402fe5e06a5f603e0cbcf5
Selena Solis <lenansolis@gmail.com>
2019-04-08 23:44:54 -0500

    allowed for multiple words

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,10 +9,8 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
-  //trims input word
-  word = word.trim();
 
-  //converts word to lowercase
+  //converts input word to lowercase
   word = word.toLowerCase();
 
   //array of vowels
@@ -21,35 +19,132 @@ function pigLatin(word) {
   //variable to hold letters of word until vowel
   let vowel = "b";
 
-  //variable to count position of vowel
-  let i = 0;
 
-  //while loop to look through word until a vowel
+  //get word count of input
+  //creates array of words splitting at any spaces
+  let words = word.split(' ');
+
+  //removes empty strings from 'words' array
+  words = words.filter(Boolean);
+
+  //counts words in 'words' array
+  let wordCount = words.length;
+
+
+  //performs action when iput has only one word
+  if (wordCount == 1){
+
+    //trims input word
+    word = word.trim();
+
+    //sets initial position of letter in word
+    let i = 0;
+
+
+    //while loop to check if each letter is in the vowel array
     while(!vowelArr.includes(vowel)){
+
       //assign letter at position i to vowel
       vowel = word.charAt(i);
+
       //increment position by 1
       i = i + 1;
     }
 
+
     //variable for first instance of a vowel
-    i = i - 1;
+    let vowelLoc = i - 1;
 
     //variable for the letters before the vowel
-    let beg = word.slice(0, i);
+    let beg = word.slice(0, vowelLoc);
+
+
 
     //if word starts with a vowel add "yay"
-
     if (vowelArr.includes(word.charAt(0))){
+
       //removes letters before vowel and concatenates them to the end with "yay"
-      word = word.slice(i) + beg + 'yay';
+      word = word.slice(vowelLoc) + beg + 'yay';
+
     }
+
     //if not add "ay"
     else{
+
       //removes letters before vowel and concatenates them to the end with "ay"
-      word = word.slice(i) + beg + 'ay';
+      word = word.slice(vowelLoc) + beg + 'ay';
+
     }
+
     return word;
+  }
+
+  
+  //performs action when iput has more than one word
+  if(wordCount > 1){
+
+    //define empty string for translated sentence
+    let translated = "";
+    
+
+    //loops through each word in 'words' array
+    for (let i=0; i<words.length; i++){
+
+      //assigns each word to new variable
+      let wordCycle = words[i];
+
+      //assigns 'vowel' a value that is not in the 'vowelArr' to reset
+      let vowel = 'b';
+
+      //sets initial position of letter in word
+      let j = 0;
+
+
+      //while loop to check if each letter is in the vowel array
+      while(!vowelArr.includes(vowel)){
+
+        //assign letter at position i to vowel
+        vowel = wordCycle.charAt(j);
+        //increment position by 1
+        j = j + 1;
+
+      }
+
+
+      //variable for first instance of a vowel
+      let vowelLoc = j - 1;
+
+      //variable for the letters before the vowel
+      let beg = wordCycle.slice(0, vowelLoc);
+
+
+      //if word starts with a vowel add "yay"
+      if (vowelArr.includes(wordCycle.charAt(0))){
+
+      //removes letters before vowel and concatenates them to the end with "yay"
+      wordCycle = wordCycle.slice(vowelLoc) + beg + 'yay';
+
+      }
+
+      //if not add "ay"
+      else{
+
+      //removes letters before vowel and concatenates them to the end with "ay"
+      wordCycle = wordCycle.slice(vowelLoc) + beg + 'ay';
+
+      }
+
+
+      //concatenates each translated word
+      translated = translated + wordCycle + " ";
+    
+    }
+
+    //returns final sentence
+    return translated;
+    
+  }
+
 }
 
 
@@ -81,6 +176,9 @@ if (typeof describe === 'function') {
       assert.equal(pigLatin('HeLlO '), 'ellohay');
       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
     });
+    it('should separate two words and return them together', ()=> {
+      assert.equal(pigLatin('Egg Rocket'), 'eggyay ocketray ');
+    })
   });
 } else {
 
